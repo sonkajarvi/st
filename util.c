a8a8d8e9d0f3edac1e02772f58add71d8acab1df
sonkajarvi <sonkajarvi@hotmail.com>
2024-08-19 22:18:38 +0300

    add a utility for reading files

---
+++ util.c
@@ -0,0 +1,31 @@
+#include "util.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char *read_file(const char *path)
+{
+    FILE *file = fopen(path, "r");
+    char *s = NULL;
+    size_t len = 0, ret = 0;
+
+    if (file) {
+
+        fseek(file, 0, SEEK_END);
+        len = ftell(file);
+        fseek(file, 0, SEEK_SET);
+
+        s = malloc(len + 1);
+        if (s)
+            ret = fread(s, 1, len, file);
+
+        fclose(file);
+
+        if (len != ret) {
+            free(s);
+            s = NULL;
+        }
+    }
+
+    return s;
+}
