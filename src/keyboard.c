d59460f760f3ea6d8b41534ba8603febab47c275
sonkajarvi <sonkajarvi@hotmail.com>
2024-09-13 04:57:56 +0300

    input/kb: fix issues

--- src/keyboard.c
+++ src/keyboard.c
@@ -1,34 +1,38 @@
 #include <assert.h>
 
-#include <st/common.h>
-#include <st/keys.h>
+#include <st/engine.h>
+#include <st/input/keyboard.h>
+#include <st/input/keys.h>
 
 bool key_down(int key)
 {
-    assert(key >= ST_KEY_FIRST && key <= ST_KEY_LAST);
-    assert(global_engine_context);
-    StWindow *window = global_engine_context->window;
+    assert(key >= __ST_KEY_FIRST && key <= __ST_KEY_LAST);
+    StEngine *e = engine_context();
+    assert(e);
+    StWindow *window = e->window;
     assert(window);
 
-    return window->keyboard.state[key].curr;
+    return window->keyboard.state[key].current;
 }
 
 bool key_press(int key)
 {
-    assert(key >= ST_KEY_FIRST && key <= ST_KEY_LAST);
-    assert(global_engine_context);
-    StWindow *window = global_engine_context->window;
+    assert(key >= __ST_KEY_FIRST && key <= __ST_KEY_LAST);
+    StEngine *e = engine_context();
+    assert(e);
+    StWindow *window = e->window;
     assert(window);
 
-    return key_down(key) && !window->keyboard.state[key].prev;
+    return key_down(key) && !window->keyboard.state[key].previous;
 }
 
 bool key_release(int key)
 {
-    assert(key >= ST_KEY_FIRST && key <= ST_KEY_LAST);
-    assert(global_engine_context);
-    StWindow *window = global_engine_context->window;
+    assert(key >= __ST_KEY_FIRST && key <= __ST_KEY_LAST);
+    StEngine *e = engine_context();
+    assert(e);
+    StWindow *window = e->window;
     assert(window);
 
-    return !key_down(key) && window->keyboard.state[key].prev;
+    return !key_down(key) && window->keyboard.state[key].previous;
 }
