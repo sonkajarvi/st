f85037801541ee57ea56700d32fef581111b6b51
sonkajarvi <sonkajarvi@hotmail.com>
2024-09-26 01:04:11 +0300

    print: make errors brighter

--- include/st/print.h
+++ include/st/print.h
@@ -8,7 +8,7 @@
 #define st_debug(fmt, ...)  __do_print(stdout, ST_ESC_BLACK_B"DEBUG: ", __func__, __LINE__, fmt, ##__VA_ARGS__)
 #define st_log(fmt, ...)    __do_print(stdout, ST_ESC_RESET"LOG: ", __func__, __LINE__, fmt, ##__VA_ARGS__)
 #define st_warn(fmt, ...)   __do_print(stderr, ST_ESC_YELLOW_B"WARN: ", __func__, __LINE__, fmt, ##__VA_ARGS__)
-#define st_error(fmt, ...)  __do_print(stderr, ST_ESC_RED"ERROR: ", __func__, __LINE__, fmt, ##__VA_ARGS__)
+#define st_error(fmt, ...)  __do_print(stderr, ST_ESC_RED_B"ERROR: ", __func__, __LINE__, fmt, ##__VA_ARGS__)
 
 void __do_print(FILE *fp, const char *pre, const char *func, const int line, const char *fmt, ...);
 
