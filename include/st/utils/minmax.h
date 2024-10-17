dbef76b0da8b54cc9b43a7b24999a2342acce5a2
sonkajarvi <sonkajarvi@hotmail.com>
2024-10-17 10:12:40 +0300

    utils: add min and max

---
+++ include/st/utils/minmax.h
@@ -0,0 +1,14 @@
+#ifndef ST_UTILS_MINMAX_H
+#define ST_UTILS_MINMAX_H
+
+#define st_min(a, b) ({            \
+    const __typeof__(a) __a = (a); \
+    const __typeof__(b) __b = (b); \
+    __a < __b ? __a : __b; })
+
+#define st_max(a, b) ({            \
+    const __typeof__(a) __a = (a); \
+    const __typeof__(b) __b = (b); \
+    __a > __b ? __a : __b; })
+
+#endif // ST_UTILS_MINMAX_H
