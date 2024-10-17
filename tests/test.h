5814b759cbcde880623b8e4e45c0f0d3603d43ba
sonkajarvi <sonkajarvi@hotmail.com>
2024-10-18 01:19:09 +0300

    tests: change output format, rewrite test header

--- tests/test.h
+++ tests/test.h
@@ -1,55 +1,54 @@
 #ifndef TEST_H
 #define TEST_H
 
-#define TEST_SUCCESS 0
-#define TEST_FAILURE 1
-
-#define __func(x) __func_##x
-#define __name(x) __name_##x
-
-#define __RESET     "\033[0m"
-#define __RED       "\033[1;91m"
-#define __GREEN     "\033[1;92m"
-
-#ifdef ST_TESTS_EXIT_ON_ERROR
-#define __RET return r.result;
+#ifdef ST_TESTS_EXIT_ON_FAILURE
+#define __test_return_or_not(status) if (status) return status
 #else
-#define __RET
+#define __test_return_or_not(status)
 #endif
 
-#define __run(x) \
-    do { \
-        const StTestResult r = x.function(); \
-        if (r.result == TEST_SUCCESS) { \
-            printf("[  " __GREEN "ok" __RESET "  ] %s\n", x.name); \
-        } else { \
-            printf("[ " __RED "fail" __RESET " ] %s, in '%s:%d', assert '%s'\n", x.name, r.file, r.line, r.expr); \
-            __RET \
-        } \
-    } while (0)
+#define __TEST_SUCCESS 0
+#define __TEST_FAILURE 1
 
-#define test_declare(x) \
-    do { \
-        StTestResult __func(x)(void); \
-        StTest __name(x) = { __func(x), #x }; \
-        __run(__name(x)); \
-    } while (0)
+#define __TEST_FUNCTION(x) __func_##x
+#define __TEST_NAME(x) __name_##x
 
-#define test_case(x) \
-    StTestResult __func(x)(void)
+#include <st/utils/terminal.h>
 
-#define test_assert(expr) \
-    if (!(expr)) return (StTestResult){TEST_FAILURE, __LINE__, #expr, __FILE__}
+#define __TEST_SUCCESS_FMT \
+    ST_ESC_YELLOW "%s:" \
+    ST_ESC_BOLD ST_ESC_GREEN_B " passed\n" ST_ESC_RESET
 
-#define test_success() \
-    return (StTestResult){TEST_SUCCESS, 0, 0, 0}
+#define __TEST_FAILURE_FMT \
+    ST_ESC_YELLOW "%s:"\
+    ST_ESC_BOLD ST_ESC_RED_B " failed" \
+    ST_ESC_RESET ST_ESC_BLACK_B " (assert '%s', in %s:%d)\n" ST_ESC_RESET
+
+#define __test_run(test) ({ \
+    const StTestResult __result = test.function(); \
+    if (__result.status == __TEST_SUCCESS) \
+        printf(__TEST_SUCCESS_FMT, test.name); \
+    else \
+        printf(__TEST_FAILURE_FMT, \
+            test.name, __result.expr, __result.file, __result.line); \
+    __test_return_or_not(__result.status); }) \
+
+#define test_declare(name) ({ \
+    StTestResult __TEST_FUNCTION(name)(void); \
+    StTest __TEST_NAME(name) = { __TEST_FUNCTION(name), #name }; \
+    __test_run(__TEST_NAME(name)); })
+
+#define test_case(x) StTestResult __TEST_FUNCTION(x)(void)
+
+#define test_assert(expr) if (!(expr)) \
+    return (StTestResult){ __TEST_FAILURE, __LINE__, #expr, __FILE__ }
+
+#define test_success() return (StTestResult){ __TEST_SUCCESS, 0, 0, 0 }
 
 typedef struct StTestResult
 {
-    int result;
-    int line;
-    const char *expr;
-    const char *file;
+    int status, line;
+    const char *expr, *file;
 } StTestResult;
 
 typedef struct StTest
