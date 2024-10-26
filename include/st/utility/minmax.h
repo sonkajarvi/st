a456aa99f97b98493f1c300100ab17f304dd7259
sonkajarvi <sonkajarvi@hotmail.com>
2024-10-26 22:13:14 +0300

    utility: add minmax

--- include/st/utility/minmax.h
+++ include/st/utility/minmax.h
@@ -11,4 +11,10 @@
     const __typeof__(b) __b = (b); \
     __a > __b ? __a : __b; })
 
+#define st_minmax(a, v, b) ({      \
+    const __typeof__(a) ___a = (a); \
+    const __typeof__(v) ___v = (v); \
+    const __typeof__(b) ___b = (b); \
+    st_max(___a, st_min(___v, ___b)); })
+
 #endif // ST_UTILITY_MINMAX_H
