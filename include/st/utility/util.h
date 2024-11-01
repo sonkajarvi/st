7a64c630c21f5f9944bcb764a32498b087c3d92c
sonkajarvi <sonkajarvi@hotmail.com>
2024-11-01 20:33:27 +0200

    utility: add st_container_of

--- include/st/utility/util.h
+++ include/st/utility/util.h
@@ -1,6 +1,14 @@
 #ifndef ST_UTILITY_UTIL_H
 #define ST_UTILITY_UTIL_H
 
+#include <stddef.h> // offsetof
+
+// todo: Move somewhere else
+#define st_container_of(ptr, type, member) ({             \
+    const __typeof__(((type *)0)->member) *__ptr = (ptr); \
+    (type *)((char *)__ptr - offsetof(type, member));     \
+})
+
 char *read_file(const char *path);
 
 #endif // ST_UTILITY_UTIL_H
