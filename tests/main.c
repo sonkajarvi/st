204f156443be7106d18744a0c3184f50cf20af2c
sonkajarvi <sonkajarvi@hotmail.com>
2024-11-07 18:01:09 +0200

    build+tests: remove unused sources

--- tests/main.c
+++ tests/main.c
@@ -27,36 +27,4 @@ int main()
     test_declare(st_vector_shrink);
     test_declare(st_vector_clear);
     test_declare(st_vector_free);
-
-    // Args parser
-    test_declare(args_short_option);
-    test_declare(args_short_option_combined);
-    test_declare(args_short_option_args);
-    test_declare(args_short_option_args_combined);
-    test_declare(args_long_option);
-    test_declare(args_long_option_args);
-    test_declare(args_fixed);
-
-    test_declare(args_short_option_args_array);
-    test_declare(args_short_option_args_array_combined);
-    test_declare(args_short_option_args_array_next);
-    test_declare(args_short_option_args_array_trailing);
-    test_declare(args_long_option_args_array);
-    test_declare(args_long_option_args_array_next);
-
-    test_declare(args_array_escaped_separator);
-    test_declare(args_array_escaped_separators);
-    test_declare(args_array_escaped_separator_end);
-    test_declare(args_empty);
-    test_declare(args_short_option_no_id);
-    test_declare(args_long_option_no_id);
-    test_declare(args_short_option_args_no_args);
-    test_declare(args_long_option_args_no_args);
-    test_declare(args_long_option_args_no_args2);
-
-    // String builder
-    test_declare(st_strbuilder_free);
-    test_declare(st_strbuilder_append);
-    test_declare(st_strbuilder_insert);
-    test_declare(st_strbuilder_empty);
 }
