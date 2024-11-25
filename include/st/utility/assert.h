473e6f76dde001daeabd06de5ca2f6ebd1c6c63b
sonkajarvi <sonkajarvi@hotmail.com>
2024-11-25 18:44:31 +0200

    utility/assert: fix errors from print changes

--- include/st/utility/assert.h
+++ include/st/utility/assert.h
@@ -3,12 +3,12 @@
 
 #include <st/utility/compiler.h>
 
-#define st_assert(x) ({ \
+#define st_assert(x) ({  \
     if (!st_unlikely(x)) \
-        _st_assert_exit(#x, __func__, __LINE__, __FILE__); \
-})
+        __st_assert_exit(#x, __FILE_NAME__, __func__, __LINE__); })
 
-ST_NORETURN void _st_assert_exit(const char *expr,
-    const char *func, const int line, const char *file);
+ST_NORETURN
+void __st_assert_exit(const char *const expr, const char *const file,
+                      const char *const func, const int line);
 
 #endif // ST_UTILITY_ASSERT_H
--- src/assert.c
+++ src/assert.c
@@ -3,12 +3,9 @@
 #include <st/utility/assert.h>
 #include <st/utility/print.h>
 
-ST_NORETURN void _st_assert_exit(const char *expr,
-    const char *func, const int line, const char *file)
+void __st_assert_exit(const char *const expr, const char *const file,
+                      const char *const func, const int line)
 {
-    st_fprint(stderr,
-        ST_PRINT_ASSERT "'%s', in %s(), at %s:%d\n",
-        expr, func, file, line);
-
-    exit(1);
+    st_print(ST_PRINT_ASSERT, file, func, line, "\"%s\"\n", expr);
+    abort();
 }
