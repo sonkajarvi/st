f100341fff78d1640e17074361ba00255fa6a042
sonkajarvi <sonkajarvi@hotmail.com>
2024-12-07 02:35:20 +0200

    util/print: don't print milliseconds

--- src/print.c
+++ src/print.c
@@ -6,25 +6,13 @@
 #include <st/util/print.h>
 #include <st/util/terminal.h>
 
-static const char *const __PREFIXES[] = {
+static const char *const prefixes[] = {
     ST_ESC_BLACK_B "debug:",
     "log:",
     ST_ESC_YELLOW_B "*warn*:",
     ST_ESC_BOLD ST_ESC_RED_B "*ERROR*:",
     ST_ESC_BOLD ST_ESC_MAGENTA_B " **ASSERT FAILED**:"
 };
-
-static inline void print_time(FILE *const fp)
-{
-    time_t t = time(NULL);
-    struct tm *tm = localtime(&t);
-    struct timespec ts;
-    clock_gettime(CLOCK_REALTIME, &ts);
-
-    fprintf(fp, ST_ESC_RESET ST_ESC_GREEN "[%02d:%02d:%02d.%03ld]" ST_ESC_RESET,
-        tm->tm_hour, tm->tm_min, tm->tm_sec,
-        ((ts.tv_sec * 1000) + (ts.tv_nsec / 1000)) % 1000);
-}
 
 void st_print(const int level, const char *const file, const char *const func,
               const int line, const char *const fmt, ...)
@@ -33,10 +21,13 @@ void st_print(const int level, const char *const file, const char *const func,
     if (level >= ST_PRINT_WARN)
         fp = stderr;
 
-    print_time(fp);
-    fprintf(fp, ST_ESC_YELLOW " %s:%s:%d:" ST_ESC_RESET, file, func, line);
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
+    fprintf(fp, ST_ESC_RESET ST_ESC_GREEN "[%02d:%02d:%02d]" ST_ESC_RESET,
+        tm->tm_hour, tm->tm_min, tm->tm_sec);
 
-    fprintf(fp, " %s ", __PREFIXES[level]);
+    fprintf(fp, ST_ESC_YELLOW " %s @ %s:%d" ST_ESC_RESET, func, file, line);
+    fprintf(fp, " %s ", prefixes[level]);
 
     va_list args;
     va_start(args, fmt);
