aab527e4e3b0735a4a25823218efb542d9e95744
sonkajarvi <sonkajarvi@hotmail.com>
2024-12-12 03:21:06 +0200

    util/print: add st_cont() for continued printing

--- include/st/util/print.h
+++ include/st/util/print.h
@@ -6,6 +6,7 @@
 #define ST_PRINT_WARN   2
 #define ST_PRINT_ERROR  3
 #define ST_PRINT_ASSERT 4
+#define ST_PRINT_CONT   5
 
 #define __ST_PRINT_HELPER(level, fmt, ...) \
     st_print(level, __FILE_NAME__, __func__, __LINE__, fmt, ##__VA_ARGS__)
@@ -14,9 +15,10 @@
 #define st_log(fmt, ...)   __ST_PRINT_HELPER(ST_PRINT_LOG, fmt, ##__VA_ARGS__)
 #define st_warn(fmt, ...)  __ST_PRINT_HELPER(ST_PRINT_WARN, fmt, ##__VA_ARGS__)
 #define st_error(fmt, ...) __ST_PRINT_HELPER(ST_PRINT_ERROR, fmt, ##__VA_ARGS__)
+#define st_cont(fmt, ...)  __ST_PRINT_HELPER(ST_PRINT_CONT, fmt, ##__VA_ARGS__)
 
 __attribute__((format(printf, 5, 6)))
-void st_print(const int level, const char *const file, const char *const func,
+void st_print(int level, const char *const file, const char *const func,
               const int line, const char *const fmt, ...);
 
 #endif // ST_UTILITY_PRINT_H
--- src/print.c
+++ src/print.c
@@ -6,33 +6,57 @@
 #include <st/util/print.h>
 #include <st/util/terminal.h>
 
-static const char *const prefixes[] = {
-    ST_ESC_BLACK_B "debug:",
-    "log:",
-    ST_ESC_YELLOW_B "*warn*:",
-    ST_ESC_BOLD ST_ESC_RED_B "*ERROR*:",
-    ST_ESC_BOLD ST_ESC_MAGENTA_B " **ASSERT FAILED**:"
+static int last_level = -1;
+
+static const char *const color_prefixes[] = {
+    ST_ESC_BLACK_B,
+    "",
+    ST_ESC_YELLOW_B,
+    ST_ESC_BOLD ST_ESC_RED_B,
+    ST_ESC_BOLD ST_ESC_MAGENTA_B
 };
 
-void st_print(const int level, const char *const file, const char *const func,
+static const char *const level_prefixes[] = {
+    "debug:",
+    "log:",
+    "*warn*:",
+    "*ERROR*:",
+    " **ASSERT FAILED**:"
+};
+
+void st_print(int level, const char *const file, const char *const func,
               const int line, const char *const fmt, ...)
 {
+    va_list args;
     FILE *fp = stdout;
+
+    if (level == ST_PRINT_CONT) {
+        st_assert(last_level != -1 && "Nothing to continue printing from");
+
+        if ((level = last_level) >= ST_PRINT_WARN)
+            fp = stderr;
+        goto cont;
+    }
+
     if (level >= ST_PRINT_WARN)
         fp = stderr;
 
     time_t t = time(NULL);
     struct tm *tm = localtime(&t);
-    fprintf(fp, ST_ESC_RESET ST_ESC_GREEN "[%02d:%02d:%02d]" ST_ESC_RESET,
-        tm->tm_hour, tm->tm_min, tm->tm_sec);
+    fprintf(fp, ST_ESC_RESET ST_ESC_GREEN "[%02d:%02d:%02d]" ST_ESC_YELLOW " %s @ %s:%d" ST_ESC_RESET " %s%s ",
+        tm->tm_hour, tm->tm_min, tm->tm_sec,
+        func, file, line,
+        color_prefixes[level], level_prefixes[level]);
+    goto no_cont;
 
-    fprintf(fp, ST_ESC_YELLOW " %s @ %s:%d" ST_ESC_RESET, func, file, line);
-    fprintf(fp, " %s ", prefixes[level]);
+cont:
+    fprintf(fp, "%s", color_prefixes[level]);
 
-    va_list args;
+no_cont:
     va_start(args, fmt);
     vfprintf(fp, fmt, args);
     va_end(args);
 
     fprintf(fp, "%s", ST_ESC_RESET);
+    last_level = level;
 }
